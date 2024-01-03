19c8e3f84def81a9857589e4240826542eddd1ab
Vidhusha_A <vaperumal@americanfirstfinance.com>
2024-01-03 15:10:04 +0530

    git commands for task

---
+++ src/main/java/AddFile.java
@@ -0,0 +1,2 @@
+public class AddFile {
+}
--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -1,6 +1,7 @@
 public class FirstProgram {
     public static void main(String args[]){
         System.out.println("This is a sample program for first commit");
+
     }
 
 }
